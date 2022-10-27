852d3eb05d52383ac97302d0e6cdbafb5f24e676
theblackdev <61018624+marcSitze@users.noreply.github.com>
2022-10-27 14:11:29 +0100

    Update config.ts

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,6 +1,6 @@
 export const config = {
-    API_KEY: process.env.API_KEY || "20950d0be8666fd55d05670638edc1ac",
-    API_SECRET_KEY: process.env.API_SECRET_KEY || "5ebb95e2f52cea1a0b2e1e5562fd5d31",
+    API_KEY: process.env.API_KEY || "",
+    API_SECRET_KEY: process.env.API_SECRET_KEY || "",
     SHOP: process.env.SHOP || "wembe-test.myshopify.com",
     SCOPES: process.env.SCOPES || "write_products",
     HOST: process.env.HOST || "https://c3df-51-195-44-15.eu.ngrok.io",
@@ -8,6 +8,6 @@ export const config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     quable: {
         HOST: process.env.QUABLE_HOST || 'https://test-case.quable.com',
-        TOKEN: process.env.QUABLE_TOKEN || '055CF6DA40695A92B87396DED02638AA8E2A3D14'
+        TOKEN: process.env.QUABLE_TOKEN || ''
     }
 }
